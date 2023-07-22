3fe955d0637331c9929f4293ccce6eba79cb9cd9
Prashan Samarathunge <prashan@team.bhasha.lk>
2023-07-22 08:24:05 +0530

    Added `MenuItem` Structure

---
+++ Little Lemon dinner menu/Models/MenuItem.swift
@@ -0,0 +1,72 @@
+//
+//  MenuItem.swift
+//  Little Lemon dinner menu
+//
+//  Created by Prashan Samarathunge on 2023-07-22.
+//
+
+import Foundation
+
+
+struct MenuItem{
+    
+    //MARK: TypeAlias
+    
+    
+    //MARK: - Enum
+    enum Stars{
+        case NoStars
+        case Stars(Int)
+        
+        var description:String{
+            switch self{
+            case .NoStars:
+                return "Not Rated Yet"
+            case .Stars(let stars):
+                return "Rated with \(stars)"
+            }
+        }
+    }
+    
+    
+    //MARK: - Classes
+    
+    
+    
+    //MARK: - Structs
+    
+    
+    
+    //MARK: - Constants
+    
+    
+    
+    //MARK: - Variables
+    var id:Int
+    var name:String
+    var description:String
+    var imageName:String
+    var stars:Stars = .NoStars
+    var liked:Bool = false
+   
+    
+    
+}
+
+extension MenuItem:Hashable{
+    static func == (lhs: MenuItem, rhs: MenuItem) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+extension MenuItem{
+    
+    public mutating func likeUnLike(){
+        self.liked = !self.liked
+    }
+    
+    public mutating func changeStars(_ stars:Stars){
+        self.stars = stars
+    }
+    
+}
