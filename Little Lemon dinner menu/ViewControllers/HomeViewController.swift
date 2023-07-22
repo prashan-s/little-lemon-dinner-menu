ce146b9d1529f51a99508b791570276f4a386d6e
Prashan Samarathunge <prashan@team.bhasha.lk>
2023-07-22 08:23:47 +0530

    Renamed `ViewController` to `HomeViewController`.

---
+++ Little Lemon dinner menu/ViewControllers/HomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Little Lemon dinner menu
+//
+//  Created by Prashan Samarathunge on 2023-07-22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+
+    
+    //MARK: TypeAlias
+    
+    
+    //MARK: - Enum
+    
+    
+    
+    //MARK: - Classes
+    
+    
+    
+    //MARK: - Structs
+    
+    
+    
+    //MARK: - Constants
+    
+    
+    
+    //MARK: - Variables
+    var menu:Menu = .init()
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //MARK: - VC Life Cycle
+
+    override func viewDidLoad() {
+        
+    }
+}
+
